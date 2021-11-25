73b813cf41a403f2126a32b04dc40c758b5313f1
Rob <iamrobertjeffrey@gmail.com>
2021-11-25 16:04:07 +0000

    added time limit to game screen.

--- frontend/src/components/GameScreen.jsx
+++ frontend/src/components/GameScreen.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import Word from './Word'
 import { UserInput } from '../styles/TypeGame.styled'
-import { useContext } from 'react'    
+import { useContext } from 'react'
 import GameContext from '../context/GameContext'
 
 const GameScreen = () => {
-    const {currentTime} = useContext(GameContext)
-    const {score} = useContext(GameContext)
-    const {currentWord} = useContext(GameContext)
-    const {currentInput} = useContext(GameContext)
-    const {setCurrentInput} = useContext(GameContext)
+    const { currentTime } = useContext(GameContext)
+    const { score } = useContext(GameContext)
+    const { currentWord } = useContext(GameContext)
+    const { currentInput } = useContext(GameContext)
+    const { setCurrentInput } = useContext(GameContext)
+    const { currentTimeLimit } = useContext(GameContext)
     return (
         <>
-            <Word currentTime={currentTime} score={score} currentWord={currentWord}/>
-            <UserInput autoFocus type="text" value={currentInput} onChange={(e) => setCurrentInput(e.target.value)}/>
+            <Word currentTime={currentTime} score={score} currentWord={currentWord} />
+            <UserInput autoFocus type="text" value={currentInput} onChange={(e) => setCurrentInput(e.target.value)} />
+            <h2>{currentTimeLimit / 10} second limit per word!</h2>
         </>
     )
 }
