393fa8b1324ffe4f66c56645834b93969a721ea8
Rob <iamrobertjeffrey@gmail.com>
2021-11-26 20:42:25 +0000

    changed cors to match hosted site again.

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,8 @@ mongoose.connect(process.env.MDB_CONNECT, (err) =>
 });
 
 app.use(cors({
-
+	origin: "https://roberts-typing.web.app",
+	credentials: true,
 	exposedHeaders: {
 		"Access-Control-Allow-Origin": "*"
 	}
